a323e11e03681622267ec09d934117d706e86d14
Alejandro Dominichini <adominichini@gmail.com>
2019-04-04 01:58:40 -0300

    Minion version

---
+++ Assets/Scripts/HealthMonitor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthMonitor : MonoBehaviour
+{
+
+    public static int HealthValue;
+    public int InternalHealth;
+    public GameObject Heart1;
+    public GameObject Heart2;
+    public GameObject Heart3;
+    public GameObject VictoryMessage;
+
+
+    void Start()
+    {
+        HealthValue = 0;
+    }
+
+
+    void Update()
+    {
+        InternalHealth = HealthValue;
+
+        if (HealthValue == 1)
+        {
+            Heart1.SetActive(true);
+        }
+        if (HealthValue == 2)
+        {
+            Heart2.SetActive(true);
+        }
+        if (HealthValue == 3)
+        {
+            Heart3.SetActive(true);
+            VictoryMessage.SetActive(true);
+        }
+
+    }
+}
---
+++ Assets/Scripts/HeartCollect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartCollect: MonoBehaviour {
+
+    public int rotateSpeed;
+    public AudioSource collectSound;
+    public GameObject thisHeart;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotateSpeed = 2;
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        collectSound.Play();
+        HealthMonitor.HealthValue += 1;
+        thisHeart.SetActive(false);
+    }
+}
